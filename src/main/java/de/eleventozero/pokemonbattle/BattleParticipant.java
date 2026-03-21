27f7d4911c43f555562db071904ec6a413e3a3e3
eleventozero <christiansauerwald@posteo.de>
2026-03-21 13:12:19 +0100

    feat: add BattleParticipant core features

--- src/main/java/de/eleventozero/pokemonbattle/BattleParticipant.java
+++ src/main/java/de/eleventozero/pokemonbattle/BattleParticipant.java
@@ -1,8 +1,27 @@
 package de.eleventozero.pokemonbattle;
 
 /**
- * BattleParticipant is used as a Data-Type for 'Battle'.
+ * Represents a participant in a battle (player or enemy).
+ * Holds the currently active Pokemon.
  */
 
 public class BattleParticipant {
+
+	private Pokemon activePokemon;
+
+	public BattleParticipant(Pokemon activePokemon){
+		this.activePokemon = activePokemon;
+	}
+
+	// Getter
+	public Pokemon getActivePokemon(){
+		return activePokemon;
+	}
+
+	// Setter
+	public void setActivePokemon(Pokemon activePokemon){
+		this.activePokemon = activePokemon;
+	}
+
+
 }
