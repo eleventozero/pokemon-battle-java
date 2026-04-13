c38821e15934e09956019494b12d23de9f38abfa
eleventozero <christiansauerwald@posteo.de>
2026-04-13 20:19:15 +0200

    add: testLoadAttacks

--- src/test/java/de/eleventozero/pokemonbattle/testPokemonData.java
+++ src/test/java/de/eleventozero/pokemonbattle/testPokemonData.java
@@ -1,11 +1,50 @@
 package de.eleventozero.pokemonbattle;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class testPokemonData {
 
-	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.dir"));
+	@Test
+	public void testLoadPokemon_CreatePokemonProfile_Successful() {
+		PokemonProfile pikachu = PokemonData.loadPokemon( 25 );
 
-		PokemonProfile p = PokemonData.loadPokemon(1);
-		System.out.println(p.getName());
+		Attack thunderShock = new Attack( "Thunder Shock", 40, "electric" );
+		Attack thunderbolt = new Attack( "Thunderbolt", 90, "electric" );
+		Attack explosion = new Attack( "Explosion", 170, "normal" );
+
+		Attack[] attacks = { thunderShock, thunderbolt, explosion };
+
+		assertEquals( "Pikachu", pikachu.getName() );
+		assertEquals( 35, pikachu.getMaxHp() );
+		assertEquals( 55, pikachu.getAttack() );
+		assertEquals( 40, pikachu.getDefense() );
+		assertEquals( "electric", pikachu.getType() );
+		//assertArrayEquals( attacks, pikachu.getAttacks() );
 	}
+
+	@Test
+	public void testLoadAttacks_CreateAttackArray_Successful() {
+		Attack thunderShock = new Attack( "Thunder Shock", 40, "electric" );
+		Attack thunderbolt = new Attack( "Thunderbolt", 90, "electric" );
+		Attack explosion = new Attack( "Explosion", 170, "normal" );
+		Attack[] attacks = { thunderShock, thunderbolt, explosion };
+
+		PokemonProfile pikachu = PokemonData.loadPokemon( 25 );
+		Attack[] pikachuAttacks = pikachu.getAttacks();
+
+		assertEquals("Thunder Shock", pikachuAttacks[0].getName() );
+		assertEquals(40, pikachuAttacks[0].getDamage() );
+		assertEquals("electric", pikachuAttacks[0].getType() );
+
+		assertEquals("Thunderbolt", pikachuAttacks[1].getName() );
+		assertEquals(90, pikachuAttacks[1].getDamage() );
+		assertEquals("electric", pikachuAttacks[1].getType() );
+
+		assertEquals("Explosion", pikachuAttacks[2].getName() );
+		assertEquals(170, pikachuAttacks[2].getDamage() );
+		assertEquals("normal", pikachuAttacks[2].getType() );
+	}
+
 }
--- src/test/java/de/eleventozero/pokemonbattle/testPokemonProfile.java
+++ src/test/java/de/eleventozero/pokemonbattle/testPokemonProfile.java
@@ -15,13 +15,13 @@ public class testPokemonProfile {
 
 		Attack[] attacks = { thunderShock, thunderbolt, explosion };
 
-		PokemonProfile pp = new PokemonProfile( "Pikachu", 35, 55, 44, "electric",
+		PokemonProfile pp = new PokemonProfile( "Pikachu", 35, 55, 40, "electric",
 				attacks );
 
 		assertEquals( "Pikachu", pp.getName() );
 		assertEquals( 35, pp.getMaxHp() );
 		assertEquals( 55, pp.getAttack() );
-		assertEquals( 44, pp.getDefense() );
+		assertEquals( 40, pp.getDefense() );
 		assertEquals( "electric", pp.getType() );
 		assertArrayEquals( attacks, pp.getAttacks() );
 	}
