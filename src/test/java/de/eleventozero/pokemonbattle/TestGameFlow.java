6b6c74e91f9e34575ab94212de2475e602851c29
eleventozero <christiansauerwald@posteo.de>
2026-04-13 21:43:32 +0200

    add: TestGameFlow

--- src/test/java/de/eleventozero/pokemonbattle/TestGameFlow.java
+++ src/test/java/de/eleventozero/pokemonbattle/TestGameFlow.java
@@ -1,4 +1,24 @@
 package de.eleventozero.pokemonbattle;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class TestGameFlow {
+
+	@Test
+	public void testGameFlow_Constructor_Successful() {
+		Pokemon p = new Pokemon( PokemonData.loadPokemon(25));
+		BattleParticipant player = new BattleParticipant( p );
+
+		Pokemon c = new Pokemon( PokemonData.loadPokemon(6));
+		BattleParticipant enemy = new BattleParticipant( c );
+
+		GameFlow g = new GameFlow( player, enemy);
+
+		assertEquals(player, g.getPlayer());
+		assertEquals(enemy, g.getEnemy());
+	}
+
+
 }
